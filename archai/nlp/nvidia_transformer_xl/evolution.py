added baselines to evolution.py + fixed pareto calculation

archai/nlp/nvidia_transformer_xl/evolution.py
@@ -4,6 +4,7 @@ import re
 import pickle
 import copy
 import imageio
+from numpy.lib.arraysetops import isin
 import yaml
 import time
 import types
@@ -39,17 +40,23 @@ class Converter(object):
         gene.append(config['d_model'])
         gene.append(sample_n_layer)
 
-        for i in range(self.max_n_layer):
-            if i < sample_n_layer:
-                gene.append(config['d_inner'][i])
+        for i in range(max(self.max_n_layer, sample_n_layer)):
+            if isinstance(config['d_inner'], list):
+                if i < sample_n_layer:
+                    gene.append(config['d_inner'][i])
+                else:
+                    gene.append(config['d_inner'][0])
             else:
-                gene.append(config['d_inner'][0])
+                gene.append(config['d_inner'])
 
-        for i in range(self.max_n_layer):
-            if i < sample_n_layer:
-                gene.append(config['n_head'][i])
+        for i in range(max(self.max_n_layer, sample_n_layer)):
+            if isinstance(config['n_head'], list):
+                if i < sample_n_layer:
+                    gene.append(config['n_head'][i])
+                else:
+                    gene.append(config['n_head'][0])
             else:
-                gene.append(config['n_head'][0])
+                gene.append(config['n_head'])
 
         return gene
 
@@ -64,14 +71,13 @@ class Converter(object):
         current_index += 1
 
         config['d_inner'] = gene[current_index: current_index + config['n_layer']]
-        current_index += self.max_n_layer
+        current_index += max(self.max_n_layer, config['n_layer'])
 
         config['n_head'] = gene[current_index: current_index + config['n_layer']]
-        current_index += self.max_n_layer
+        current_index += max(self.max_n_layer, config['n_layer'])
 
         return config
 
-    
     def gene2key(self, gene):
         key_list = []
 
@@ -1072,17 +1078,18 @@ def submit_gt_jobs(args, max_step=500, start_config=0, bundle_count=50, n_gpus=8
     create_jobs(all_population, start_config, bundle_count, max_step, n_gpus, gpu_config, targets[0], exp_name='evolution_', is_pareto=is_pareto)
 
 
-def get_diff_with_pareto(gt_latencies, gt_val_ppls, is_gt_pareto, is_proxy_pareto):
+def get_diff_with_pareto(gt_latencies, gt_val_ppls, is_gt_pareto, is_proxy_pareto, min_acceptable_latency_diff=0):
     sorted_idx = np.argsort(gt_latencies)
     
     ppl_diff = []
+    on_pareto = 0
     for i, idx in enumerate(sorted_idx):
         latency, val_ppl = gt_latencies[idx], gt_val_ppls[idx]
         if not is_proxy_pareto[idx]:
             continue
         if is_gt_pareto[idx]:
-            pass
             ppl_diff.append(0.)
+            on_pareto += 1
         else:
             idx_fwd, idx_bkwd = None, None
             for j in sorted_idx[i+1:]:
@@ -1102,15 +1109,24 @@ def get_diff_with_pareto(gt_latencies, gt_val_ppls, is_gt_pareto, is_proxy_paret
                 closest_idx = idx_fwd
             else:
                 closest_idx = idx_bkwd
-            print('latency difference with closest pareto point: {:1f} ms'.format(np.absolute(latency-gt_latencies[closest_idx])*1000))
-            ppl_diff.append(np.absolute(val_ppl-gt_val_ppls[closest_idx])*100./gt_val_ppls[closest_idx])        
+
+            latency_diff = np.absolute(latency-gt_latencies[closest_idx])*1000
+            print('latency difference with closest pareto point: {:1f} ms'.format(latency_diff))
+            
+            if latency_diff <= min_acceptable_latency_diff:
+                ppl_diff.append(np.absolute(val_ppl-gt_val_ppls[closest_idx])*100./gt_val_ppls[closest_idx])        
     
-    assert len(ppl_diff)==np.sum(is_proxy_pareto)
+    if min_acceptable_latency_diff==0:
+        assert len(ppl_diff)==np.sum(is_proxy_pareto)
+    
+    print(f'{on_pareto} points out of {np.sum(is_proxy_pareto)} were on the ground-truth pareto')
+
     return np.mean(ppl_diff)
 
 
-def get_gt_pareto(args, exp_name, path_to_dir, start_config, ppl_eps=0.1, latency_eps = 0.01, hybrid=False, use_convex_hull=False):
-    gt_results = gather_results(exp_name, path_to_dir, filetypes=['.yaml', '.json'], verbose=False)
+def get_gt_pareto(args, exp_name, path_to_dir, start_config, ppl_eps=0.1, latency_eps = 0.01, hybrid=False, use_convex_hull=False, 
+                    min_acceptable_latency_diff=0, baseline_exp=None):
+    gt_results = gather_results(exp_name, os.path.join(path_to_dir, exp_name), filetypes=['config.yaml', '.json'], verbose=False)
     print('found %d model configurations' % len(gt_results.keys()))
 
     print('Loading the latencies from log file')
@@ -1118,7 +1134,12 @@ def get_gt_pareto(args, exp_name, path_to_dir, start_config, ppl_eps=0.1, latenc
     path_to_pkl = os.path.join(results_path, 'latencies.pkl')
     with open(path_to_pkl, 'rb') as f:
         latencies = pickle.load(f)
-    
+    print(len(latencies.keys()))
+
+    if baseline_exp is not None:
+        print('Loading the baseline')
+        latencies_baseline, val_ppls_baseline = analyze_baseline(args, exp_name=baseline_exp, path_to_dir=path_to_dir)
+
     # load previous pareto
     loaded_pareto = None
     fname = 'pareto{}'.format('' if hybrid else '_params')
@@ -1206,18 +1227,22 @@ def get_gt_pareto(args, exp_name, path_to_dir, start_config, ppl_eps=0.1, latenc
     TPR = len(np.intersect1d(np.nonzero(is_gt_pareto)[0], np.nonzero(is_pareto)[0]))*100./len(np.nonzero(is_gt_pareto)[0])
     TNR = len(np.intersect1d(np.nonzero(~is_gt_pareto)[0], np.nonzero(~is_pareto)[0]))*100./len(np.nonzero(~is_gt_pareto)[0])
     print(f'TPR={TPR}% and TNR={TNR}%')
-    mean_ppl_difference = get_diff_with_pareto(gt_latencies, gt_val_ppls, is_gt_pareto, is_pareto)
+    mean_ppl_difference = get_diff_with_pareto(gt_latencies, gt_val_ppls, is_gt_pareto, is_pareto, min_acceptable_latency_diff=min_acceptable_latency_diff)
     print('mean ppl difference between proxy and gt pareto: {:.1f}%'.format(mean_ppl_difference))
     
     plt.figure()
     plt.scatter(np.asarray(gt_latencies)[~is_pareto] * 1000., np.asarray(gt_val_ppls)[~is_pareto], s=5)
     plt.scatter(np.asarray(gt_latencies)[is_pareto] * 1000., np.asarray(gt_val_ppls)[is_pareto], s=5)
-    plt.scatter(np.asarray(gt_latencies)[is_gt_pareto] * 1000., np.asarray(gt_val_ppls)[is_gt_pareto], s=5)
+    # plt.scatter(np.asarray(gt_latencies)[is_gt_pareto] * 1000., np.asarray(gt_val_ppls)[is_gt_pareto], s=5)
     # plt.scatter(np.asarray(gt_val_ppls)[~is_pareto], np.asarray(gt_latencies)[~is_pareto] * 1000., s=10)
     # plt.scatter(np.asarray(gt_val_ppls)[is_pareto], np.asarray(gt_latencies)[is_pareto] * 1000., s=10)
+    if baseline_exp:
+        plt.scatter(np.asarray(latencies_baseline) * 1000., np.asarray(val_ppls_baseline), s=10, marker='*', c='red')
+        plt.xlim((min(np.min(gt_latencies), np.min(latencies_baseline))*1000-10, np.max(gt_latencies)*1000+10))
+    else:
+        plt.xlim(np.min(gt_latencies)*1000-10, np.max(gt_latencies)*1000+10)
     plt.xlabel('Latency (ms)')
-    plt.ylabel('Val PPL')
-    plt.xlim((np.min(gt_latencies)*1000-10, np.max(gt_latencies)*1000+10))
+    plt.ylabel('Val PPL')  
     plt.title('Pareto Curve')
     plt.grid(axis='y')
     fname = 'gt_pareto_latency{}.png'.format('' if hybrid else '_params')
@@ -1369,6 +1394,64 @@ def get_final_pareto_front(args, eps=0.05, hybrid=False, use_convex_hull=False):
     plt.savefig(os.path.join(results_path, 'final_search_pareto{}.png'.format('' if hybrid else '_params')), bbox_inches="tight")
 
 
+def analyze_baseline(args, exp_name, path_to_dir):
+    alg = Evolution(**args)
+    
+    baseline_results = gather_results(exp_name, os.path.join(path_to_dir, exp_name), filetypes=['config.yaml', '.json'], verbose=False)
+    with open(os.path.join(path_to_dir, exp_name, 'latency_summary.yaml'), 'r') as f:
+        latencies = yaml.load(f)
+
+    latencies_list = []
+    val_ppls = []
+    for config_name, latency in latencies.items():
+        latencies_list.append(latency)
+        val_ppls.append(baseline_results[config_name]['valid_perplexity'])
+    
+    ''' Uncomment to masure latency locally
+    params = {}
+    latencies = {}
+    latencies_list = []
+    val_ppls = []
+    for _, result in baseline_results.items():
+        gene = alg.converter.config2gene(result)
+        config = alg.converter.gene2config(gene)
+        print(config)
+        key = alg.converter.gene2key(gene)
+
+        if key in params.keys():
+            continue
+
+        model_config = copy.deepcopy(model_config_defaults)
+        model_config.update(config)
+        model = get_model(model_config, train=False)
+        
+        curr_n_all_param, _, _, params_attention, params_ff = process_parameters(model, verbose=False)
+        params[key] = params_attention + params_ff
+        
+        latency = get_latency(model, model_config, n_threads=alg.n_threads, repeat=alg.latency_repeat)
+        latencies[key] = latency
+        latencies_list.append(latency)
+        val_ppls.append(result['valid_perplexity'])
+    path_to_save = os.path.join(path_to_dir, exp_name)
+    with open(os.path.join(path_to_save, 'params.pkl'), 'wb') as f:
+        pickle.dump(params, f)
+    with open(os.path.join(path_to_save, 'latencies.pkl'), 'wb') as f:
+        pickle.dump(latencies, f)
+    '''
+
+    print(f'summarized {len(latencies.keys())} baseline jobs')
+
+    plt.figure()
+    plt.scatter(np.asarray(latencies_list) * 1000., val_ppls, s=5)
+    plt.xlabel('Latency (ms)')
+    plt.ylabel('Val PPL')
+    plt.grid(axis='y')
+    fname = 'baseline_pareto_latency.png'
+    plt.savefig(os.path.join(args['results_path'], fname), bbox_inches="tight")
+
+    return latencies_list, val_ppls
+
+
 if __name__=='__main__':
     seed = 1111
     np.random.seed(seed)
@@ -1420,13 +1503,43 @@ if __name__=='__main__':
     # get_final_pareto_front(args, eps=eps, hybrid=hybrid, use_convex_hull=use_convex_hull)    
 
     #--------------- compare ground-truth pareto with the proxy pareto
-    gt_exp_name = 'evolution_5000'
+    gt_exp_name = 'evolution_40000'
     path_to_amlt_results = './amlt_logs'
     os.makedirs(path_to_amlt_results, exist_ok=True)
     # command = 'amlt results {} -I "*.json"  -o {} --no-md5'.format(gt_exp_name, path_to_amlt_results)
     # os.system(command)
     # command = 'amlt results {} -I "*.yaml"  -o {} --no-md5'.format(gt_exp_name, path_to_amlt_results)
     # os.system(command)
-    path_to_amlt_results = os.path.join(path_to_amlt_results, gt_exp_name)
-    get_gt_pareto(args, exp_name=gt_exp_name, path_to_dir=path_to_amlt_results, start_config=25, 
-                    ppl_eps=0.1, latency_eps=0.01, hybrid=hybrid, use_convex_hull=use_convex_hull)
+    get_gt_pareto(args, exp_name=gt_exp_name, path_to_dir=path_to_amlt_results, start_config=0, 
+                    ppl_eps=0.1, latency_eps=0.01, hybrid=hybrid, use_convex_hull=use_convex_hull, min_acceptable_latency_diff=2, baseline_exp='evolution_baselines')
+
+    #---------------- print ppl versus nparams pareto
+    # with open('amlt_logs/evolution_40000/params_summary.yaml') as f:
+    #     n_all_params = yaml.load(f)
+    # gt_results = gather_results('evolution_40000', 'amlt_logs/evolution_40000', filetypes=['.json'], verbose=False)
+
+    # params_list = []
+    # val_ppl_list = []
+    # for job_name, result in gt_results.items():
+    #     if job_name in n_all_params.keys():
+    #         params_list.append(n_all_params[job_name]['FFN'] + n_all_params[job_name]['Attn'])
+    #         val_ppl_list.append(result['valid_perplexity'])
+    
+    # max_ppl_diff = 0.
+    # for idx, p in enumerate(params_list):
+    #     for idx2, p2 in enumerate(params_list):
+    #         if abs(p-p2)*100./p < 0.1:
+    #             if abs(val_ppl_list[idx] - val_ppl_list[idx2]) > max_ppl_diff:
+    #                 max_ppl_diff = abs(val_ppl_list[idx] - val_ppl_list[idx2])
+    #                 max_idx = idx
+
+    # print(f'maximum vertical difference in val ppl={max_ppl_diff}, happend in p={params_list[idx]}')
+
+    # plt.figure()
+    # plt.scatter(params_list, val_ppl_list, s=5)
+    # plt.xlabel('# Decoder Params')
+    # plt.ylabel('Val PPL')
+    # plt.title('Pareto Curve')
+    # plt.grid(axis='y')
+    # fname = 'pareto_params.png'
+    # plt.savefig(os.path.join(args['results_path'], fname), bbox_inches="tight")

archai/nlp/nvidia_transformer_xl/gather_results.py
@@ -178,8 +178,11 @@ def get_info_from_json(json_file, step=[], type=None):
       return None
     job_desc = '{'+job_desc.group(1)+'}}'
     work_dir = json.loads(job_desc)['data']['work_dir']
-    idx_start = re.search('amlt-results', work_dir).span()[-1] + 1
-    amlt_job = work_dir[idx_start:].split('/')[0]
+    try:
+      idx_start = re.search('amlt-results', work_dir).span()[-1] + 1
+      amlt_job = work_dir[idx_start:].split('/')[0]
+    except:
+      amlt_job = None
   
     for line in lines:
       str = re.search('DLLL \{(.+?)\}', line)
@@ -227,16 +230,19 @@ def get_info_from_json(json_file, step=[], type=None):
 def get_config_name(job):
   # idx = list(re.search('config_', job).span())[0]
   # return job[idx:]
-  if 'baseline' in job:
-    dir_name = os.path.basename(os.path.dirname(job))
-    return re.search('(config_[0-9]+_[0-9]+)', dir_name).group(1)
-  elif 'similar_params_sweep' in job or 'simp' in job or 'evolution' in job:
+  if 'similar_params_sweep' in job or 'simp' in job or 'evolution' in job:
     idx =  re.search('(config_[0-9]+)', job).span()[0]
     job = job[idx:]
     config_name = job.split('/')[0]
     return config_name + '_' + job.split('/')[1]
+  
+  elif 'baseline' in job:
+    dir_name = os.path.basename(os.path.dirname(job))
+    return re.search('(config_[0-9]+_[0-9]+)', dir_name).group(1)
+  
   elif 'evo_search' in job or 'midevolution' in job:
     return job
+  
   else:
     return re.search('(config_[0-9]+)', job).group(1)
     
@@ -983,13 +989,16 @@ def main(args):
     
     legend_keys = []
     for exp_name in args.exp_name:
-      try:
-        idx = re.search('(fear_stage_1)', exp_name).span()[-1]
-      except:
-        idx = re.search('(fear_stage1)', exp_name).span()[-1]
-      legend_key = exp_name[idx+1:].split('_')[-1]
-      if len(legend_key)==0:
-        legend_key = 'homogeneous'
+      if 'fear' in exp_name:
+        try:
+          idx = re.search('(fear_stage_1)', exp_name).span()[-1]
+        except:
+          idx = re.search('(fear_stage1)', exp_name).span()[-1]
+        legend_key = exp_name[idx+1:].split('_')[-1]
+        if len(legend_key)==0:
+          legend_key = 'homogeneous'
+      else:
+        legend_key = 'heterogeneous'
       legend_keys.append(legend_key)
       
       path_to_results = os.path.join(args.results_dir, exp_name)
@@ -1071,7 +1080,7 @@ def main(args):
     for k in legend_keys:
       plt.scatter(-np.asarray(n_params[k])[sorted_ground_truth[k]], np.asarray(val_ppl_list_gt[k])[sorted_ground_truth[k]], label=k)
     plt.ylabel('Validation PPL')
-    plt.xlabel('Total nParams')
+    plt.xlabel('Decoder nParams')
     plt.title('Pareto Curve')
     plt.grid(axis='y')
     plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))

archai/nlp/nvidia_transformer_xl/generate_archs.py
@@ -31,7 +31,7 @@ generation_seed = 4568
 np.random.seed(generation_seed)
 random.seed(generation_seed)
 
-phase = 1   # 1: submit jobs (optionally with fear stage 1 activated), 2: fear stage 2, 3: baseline, 4: similar parameter exploration
+phase = 5   # 1: submit jobs (optionally with fear stage 1 activated), 2: fear stage 2, 3: baseline, 4: similar parameter exploration, 5: trasnformer-XL baseline
 activate_fear = False
 n_unfreeze = 3  # used in phase 2
 different_seeds = None #[1111,1009,1200,1234,1302,1562,2222,3334,3425,4567]
@@ -109,11 +109,11 @@ def gather_results(exp_name, path_to_dir, filetypes='.json', verbose=True):
       else:
         for ft in filetypes:
           if ft in j:
-            if ft=='.log':
+            if '.log' in ft:
               logs = get_info_from_logs(j_path, stage_1='stage_1' in exp_name)
-            elif ft=='.json':
+            elif '.json' in ft:
               logs = get_info_from_json(j_path)
-            elif ft=='.yaml':
+            elif '.yaml' in ft:
               with open(os.path.join(j_path), 'r') as f:
                 config = yaml.load(f)
               if config is None:
@@ -305,7 +305,7 @@ def multiply(value, factor):
 
 
 if __name__ == '__main__':
-  path_to_configs = os.path.join('/home/t-mojanj/Projects/archai/archai/nlp/nvidia_transformer_xl', 'configs')
+  path_to_configs = os.path.join('archai/nlp/nvidia_transformer_xl', 'configs')
   if not os.path.exists(path_to_configs):
       os.mkdir(path_to_configs)
   
@@ -487,7 +487,7 @@ if __name__ == '__main__':
 
     generate_bash_files(path_to_configs, f_name='amlt_run_fear_baseline', exp_name=exp_name)
 
-  else:
+  elif phase==4:
     n_layers = [5]
     div_vals = [4]
     # d_inners = [512, 700]
@@ -688,3 +688,56 @@ if __name__ == '__main__':
       print('######', n_configs)
       bash_start = generate_bash_files(path_to_configs, f_name='amlt_run_fear_stage1_similar_params', bash_start=bash_start, exp_name='simp')
       bash_start_config += n_configs
+
+  else:
+    n_configs = 16
+    n_gpus = 8
+    gpu_config = 'dgx1_8gpu_fp32'
+    bundle_count = 6
+    target = 'itpscusv100cl'
+
+    c = 0
+    config_idx = 0
+    while c < n_configs: 
+        with open('../archaiphilly/nv_train.yaml') as file:
+            amlt_config = yaml.safe_load(file)
+            # if c==0:
+            #   pprint.pprint(amlt_config)
+
+        amlt_config['environment']['setup'] = ['set -e -o xtrace', 'pip install --user tensorboard']
+        amlt_config['environment']['image'] = 'debadeepta/pytorch:1.7.0-cuda11.0-cudnn8-devel'
+        
+        del amlt_config['search']
+        amlt_config['jobs'] = [{}]
+        amlt_config['jobs'][0]['name'] = 'config_{}'.format(str(config_idx))
+        amlt_config['jobs'][0]['sku'] = f'G{n_gpus}'
+        amlt_config['jobs'][0]['command'] = ['set -e -o xtrace', 'pip install --user -e .']
+        
+        for i in range(1, bundle_count+1):
+          n_layer = i + c
+          exp_name = 'j' + str(i)
+
+          if n_layer > n_configs:
+            break
+          
+          amlt_config['jobs'][0]['command'].append('python -m torch.distributed.launch --nproc_per_node="%s" archai/nlp/nvidia_transformer_xl/train.py --config %s \
+                        --config_file wt103_base.yaml --n_layer %s --n_head 8 --d_model 512 --d_head 64 --d_inner 2048 --div_val 4 --experiment_name %s' \
+                        % (str(n_gpus), gpu_config, n_layer, exp_name))
+        
+        config_file = 'nv_train_'+str(config_idx)+'.yaml'
+        path_to_configs = os.path.join('archai/nlp/nvidia_transformer_xl', 'configs')
+        f_name = os.path.join(path_to_configs, config_file)
+        with open(f_name, 'w') as file:
+            yaml.dump(amlt_config, file)
+
+        c += bundle_count
+        config_idx += 1
+
+    exp_name = 'memformer_baselines'
+    bash_f_name = 'amlt_run_memformerBase.sh'
+    bash_file = os.path.join(path_to_configs, bash_f_name)
+    if os.path.exists(bash_file):
+        os.remove(bash_file)  
+    for i in range(config_idx):
+        with open(bash_file, 'a') as f:
+            f.write('amlt run --yes archai/nlp/nvidia_transformer_xl/configs/nv_train_{}.yaml {} -t {}\n'.format(i, exp_name, target))

archai/nlp/nvidia_transformer_xl/mem_transformer.py
@@ -564,8 +564,9 @@ class MemTransformerLM(nn.Module):
         d_embed = d_model if d_embed is None else d_embed
         self.d_embed = d_embed
         self.d_model = d_model
-        self.n_head = n_head
+        self.n_head = n_head[0] if isinstance(n_head, list) else n_head
         self.d_head = d_model//n_head if d_head is None else d_head
+        d_inner = d_inner[0] if isinstance(d_inner, list) else d_inner
 
         self.word_emb = AdaptiveEmbedding(n_token, d_embed, d_model, cutoffs,
                                           div_val=div_val)