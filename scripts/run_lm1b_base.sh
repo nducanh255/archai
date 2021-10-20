e342f2d3a981fc789e88d002127a5a3c1acc69d4
Debadeepta Dey <dedey@microsoft.com>
2021-10-20 16:40:52 -0700

    Very important to set eta_min = 0.0 for lm1b training otherwise the initial_lr > eta_min in cosine annealer.

--- scripts/run_lm1b_base.sh
+++ scripts/run_lm1b_base.sh
@@ -1,3 +1,5 @@
+export NCCL_P2P_LEVEL=NVL
+
 python -m torch.distributed.launch \
 --nproc_per_node=4 \
 archai/nlp/nvidia_transformer_xl/train.py \
@@ -16,9 +18,10 @@ archai/nlp/nvidia_transformer_xl/train.py \
 --warmup_step 20000 \
 --max_step 500000 \
 --lr 0.00025 \
+--eta_min 0.0 \
 --tgt_len 32 \
 --mem_len 32 \
 --eval_tgt_len 32 \
 --batch_size 224 \
 --multi_gpu ddp \
---gpu0_bsz 32 \
+--gpu0_bsz 32
