6f1f2b6112539445944270614674208150fed7e6
Sharath Girish <sgirish@terpmail.umd.edu>
2021-11-01 19:27:05 -0400

    added yamls

--- scripts/ssl/generate_net_yaml.py
+++ scripts/ssl/generate_net_yaml.py
@@ -1,41 +1,41 @@
 import yaml
 import numpy as np
 
-# resnets = {}
-# count = 92
-# for l1 in [5,6]:
-#     for l2 in [5,6]:
-#         for l3 in [2,3,4]:
-#             for l4 in [2,3,4]:
-#                 layers = [l1,l2,l3,l4]
-#                 if layers == [2,2,2,2]:
-#                     continue
-#                 cur_resnet = {
-#                               "depth": sum(layers)*3+2,
-#                               "layers": layers,
-#                               "bottleneck": True,
-#                               "hidden_dim": 2048,
-#                               "out_features": 128,
-#                               "compress": False,
-#                               "width_per_group": 64,
-#                               "groups": 1
-#                 }
-#                 resnets["resnet_v{}".format(count)] = cur_resnet
-#                 count += 1
+resnets = {}
+count = 128
+for l1 in [2,3,4]:
+    for l2 in [2,3,4]:
+        for l3 in [2,3,4]:
+            for l4 in [2,3,4]:
+                layers = [l1,l2,l3,l4]
+                if layers == [2,2,2,2]:
+                    continue
+                cur_resnet = {
+                              "depth": sum(layers)*2+2,
+                              "layers": layers,
+                              "bottleneck": False,
+                              "hidden_dim": 2048,
+                              "out_features": 128,
+                              "compress": False,
+                              "width_per_group": 64,
+                              "groups": 1
+                }
+                resnets["resnet_v{}".format(count)] = cur_resnet
+                count += 1
 
-# with open('confs/algos/simclr_resnets.yaml','a') as f:
-#     f.write("\n")
-#     for name, cur_resnet in resnets.items():
-#         depth, layers, bottleneck, hidden_dim, out_features, compress, width_per_group, groups = cur_resnet.values()
-#         f.write(f"\n{name}:\n")
-#         f.write(f"    depth: {depth}\n")
-#         f.write(f"    layers: [{layers[0]}, {layers[1]}, {layers[2]}, {layers[3]}]\n")
-#         f.write(f"    bottleneck: {bottleneck}\n")
-#         f.write(f"    hidden_dim: {hidden_dim}\n")
-#         f.write(f"    out_features: {out_features}\n")
-#         f.write(f"    compress: {compress}\n")
-#         f.write(f"    width_per_group: {width_per_group}\n")
-#         f.write(f"    groups: {groups}\n")
+with open('confs/algos/simclr_resnets.yaml','a') as f:
+    f.write("\n")
+    for name, cur_resnet in resnets.items():
+        depth, layers, bottleneck, hidden_dim, out_features, compress, width_per_group, groups = cur_resnet.values()
+        f.write(f"\n{name}:\n")
+        f.write(f"    depth: {depth}\n")
+        f.write(f"    layers: [{layers[0]}, {layers[1]}, {layers[2]}, {layers[3]}]\n")
+        f.write(f"    bottleneck: {bottleneck}\n")
+        f.write(f"    hidden_dim: {hidden_dim}\n")
+        f.write(f"    out_features: {out_features}\n")
+        f.write(f"    compress: {compress}\n")
+        f.write(f"    width_per_group: {width_per_group}\n")
+        f.write(f"    groups: {groups}\n")
 
 # cfgs: Dict[str, List[Union[str, int]]] = {
 #     'A': [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'], #vgg11 v22
@@ -181,61 +181,61 @@ import numpy as np
 
 
 
-mobilenets = {}
-count = 1
-for t in [[1,6,6,6,6,6,6]]:
-    for c in [[16,24,32,64,96,160,320]]:
-        for n in [[1,2,3,4,3,3,1]]:
-            for s in [[1,2,2,2,1,2,1]]:
-                for width_mult in np.linspace(1,3,11):
-                    width_mult = round(width_mult,1)
-                    cur_mobilenet = {
-                                "t": t,
-                                "c": c,
-                                "n": n,
-                                "s": s,
-                                "width_mult": width_mult,
-                                "hidden_dim": 2048,
-                                "out_features": 128,
-                    }
-                    mobilenets["mobilenet_v{}".format(count)] = cur_mobilenet
-                    count += 1
+# mobilenets = {}
+# count = 1
+# for t in [[1,6,6,6,6,6,6]]:
+#     for c in [[16,24,32,64,96,160,320]]:
+#         for n in [[1,2,3,4,3,3,1]]:
+#             for s in [[1,2,2,2,1,2,1]]:
+#                 for width_mult in np.linspace(1,3,11):
+#                     width_mult = round(width_mult,1)
+#                     cur_mobilenet = {
+#                                 "t": t,
+#                                 "c": c,
+#                                 "n": n,
+#                                 "s": s,
+#                                 "width_mult": width_mult,
+#                                 "hidden_dim": 2048,
+#                                 "out_features": 128,
+#                     }
+#                     mobilenets["mobilenet_v{}".format(count)] = cur_mobilenet
+#                     count += 1
 
-for t in [[1,6,6,6,6,6,6]]:
-    for c in [[16,24,32,64,96,160,320]]:
-        for n1 in [2,3]:
-            for n2 in [3,4]:
-                for n3 in [4,5]:
-                    for n4 in [3,4]:
-                        for n5 in [3,4]:
-                            for s in [[1,2,2,2,1,2,1]]:
-                                for width_mult in [1.0]:
-                                    n = [1,n1,n2,n3,n4,n5,1]
-                                    if n==[1,2,3,4,3,3,1]:
-                                        continue
-                                    width_mult = round(width_mult,1)
-                                    cur_mobilenet = {
-                                                "t": t,
-                                                "c": c,
-                                                "n": n,
-                                                "s": s,
-                                                "width_mult": width_mult,
-                                                "hidden_dim": 2048,
-                                                "out_features": 128,
-                                    }
-                                    mobilenets["mobilenet_v{}".format(count)] = cur_mobilenet
-                                    count += 1
+# for t in [[1,6,6,6,6,6,6]]:
+#     for c in [[16,24,32,64,96,160,320]]:
+#         for n1 in [2,3]:
+#             for n2 in [3,4]:
+#                 for n3 in [4,5]:
+#                     for n4 in [3,4]:
+#                         for n5 in [3,4]:
+#                             for s in [[1,2,2,2,1,2,1]]:
+#                                 for width_mult in [1.0]:
+#                                     n = [1,n1,n2,n3,n4,n5,1]
+#                                     if n==[1,2,3,4,3,3,1]:
+#                                         continue
+#                                     width_mult = round(width_mult,1)
+#                                     cur_mobilenet = {
+#                                                 "t": t,
+#                                                 "c": c,
+#                                                 "n": n,
+#                                                 "s": s,
+#                                                 "width_mult": width_mult,
+#                                                 "hidden_dim": 2048,
+#                                                 "out_features": 128,
+#                                     }
+#                                     mobilenets["mobilenet_v{}".format(count)] = cur_mobilenet
+#                                     count += 1
 
 
-with open('confs/algos/simclr_mobilenets.yaml','a') as f:
-    f.write("\n")
-    for name, cur_mobilenet in mobilenets.items():
-        t, c, n, s, width_mult, hidden_dim, out_features = cur_mobilenet.values()
-        f.write(f"\n{name}:\n")
-        f.write(f"    t: [{','.join([str(i) for i in t])}]\n")
-        f.write(f"    c: [{','.join([str(i) for i in c])}]\n")
-        f.write(f"    n: [{','.join([str(i) for i in n])}]\n")
-        f.write(f"    s: [{','.join([str(i) for i in s])}]\n")
-        f.write(f"    width_mult: {width_mult}\n")
-        f.write(f"    hidden_dim: {hidden_dim}\n")
-        f.write(f"    out_features: {out_features}\n")
+# with open('confs/algos/simclr_mobilenets.yaml','a') as f:
+#     f.write("\n")
+#     for name, cur_mobilenet in mobilenets.items():
+#         t, c, n, s, width_mult, hidden_dim, out_features = cur_mobilenet.values()
+#         f.write(f"\n{name}:\n")
+#         f.write(f"    t: [{','.join([str(i) for i in t])}]\n")
+#         f.write(f"    c: [{','.join([str(i) for i in c])}]\n")
+#         f.write(f"    n: [{','.join([str(i) for i in n])}]\n")
+#         f.write(f"    s: [{','.join([str(i) for i in s])}]\n")
+#         f.write(f"    width_mult: {width_mult}\n")
+#         f.write(f"    hidden_dim: {hidden_dim}\n")
+#         f.write(f"    out_features: {out_features}\n")
--- scripts/ssl/plot_resnets.py
+++ scripts/ssl/plot_resnets.py
@@ -1,4 +1,5 @@
 import os
+import csv
 import sys
 import yaml
 import json
@@ -8,7 +9,142 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-from archai.networks_ssl.simclr import ModelSimCLRResNet, ModelSimCLRVGGNet, ModelSimCLRViT
+from archai.networks_ssl.simclr import ModelSimCLRResNet, ModelSimCLRVGGNet, ModelSimCLRViT, ModelSimCLRMobileNet
+
+
+
+# key = 'resnet'
+key = 'mobilenet'
+with open(f'/vulcanscratch/sgirish/dummy/resnet_params.json','r') as f:
+    resnet_params = json.load(f)
+with open(f'/vulcanscratch/sgirish/dummy/mobilenet_params.json','r') as f:
+    mobilenet_params = json.load(f)
+with open('/vulcanscratch/sgirish/eval_models.csv') as csv_file:
+    csv_reader = csv.DictReader(csv_file, delimiter=',')
+    acc, dataset, model = [[] for _ in range(3)]
+    imagenet_resnet_models = {}
+    imagenet_mobilenet_models = {}
+    for i,row in enumerate(csv_reader):
+        acc.append(float(row['epoch_top1_test']))
+        name = row['Name']
+        model.append(name.strip().split('-')[-1])
+        dataset.append(name.strip().split('-')[0].split('_')[-1])
+        if name.strip().split('-')[0].split('_')[-1] == 'imagenet':
+            if 'resnet' in name.strip().split('-')[-1] and float(row['epoch_top1_test'])>54.0 and float(resnet_params[name.strip().split('-')[-1]]['params']<30):
+                imagenet_resnet_models[name.strip().split('-')[-1]] = float(row['epoch_top1_test'])
+            elif 'mobilenet' in name.strip().split('-')[-1]and float(mobilenet_params[name.strip().split('-')[-1]]['params']<3):
+                imagenet_mobilenet_models[name.strip().split('-')[-1]] = float(row['epoch_top1_test'])
+
+imagenet_models = imagenet_resnet_models if key == 'resnet' else imagenet_mobilenet_models
+params = resnet_params if key == 'resnet' else mobilenet_params
+# params = {}
+# if key == 'resnet':
+#     with open('confs/algos/simclr_resnets.yaml', 'r') as f:
+#         conf_models = yaml.load(f, Loader=yaml.Loader)
+# elif key == 'mobilenet':
+#     with open('confs/algos/simclr_mobilenets.yaml', 'r') as f:
+#         conf_models = yaml.load(f, Loader=yaml.Loader)
+# for net in imagenet_models:
+#     conf_model = conf_models[net]
+#     if key == 'resnet':
+#         model = ModelSimCLRResNet('cifar10', conf_model['depth'], conf_model['layers'], conf_model['bottleneck'],
+#                 False, conf_model['hidden_dim'], conf_model['out_features'], groups = conf_model['groups'],
+#                 width_per_group=conf_model['width_per_group'])
+#         l1, l2, l3, l4 = conf_model['layers']
+#         net_dict = {'params':sum(p.numel() for p in model.backbone.parameters() if p.requires_grad)/1e6,
+#                     'bottom_params':sum(p.numel() for n,p in model.backbone.named_parameters() \
+#                                         if p.requires_grad and ('layer1' in n or 'layer2' in n))/1e6,
+#                     'top_params':sum(p.numel() for n,p in model.backbone.named_parameters() \
+#                                         if p.requires_grad and ('layer3' in n or 'layer4' in n))/1e6,
+#                     'l1':l1, 'l2': l2, 'l3': l3, 'l4': l4}
+#         params[net] = net_dict
+#     else:
+#         inverted_residual_setting = [[conf_model['t'][i],conf_model['c'][i],conf_model['n'][i],conf_model['s'][i]] \
+#                                         for i in range(len(conf_model['t']))]
+#         model = ModelSimCLRMobileNet(hidden_dim = conf_model["hidden_dim"], out_features = conf_model["out_features"],
+#                 inverted_residual_setting = inverted_residual_setting, width_mult = conf_model['width_mult'], compress = False
+#                 )
+#         params[net] = {'params':sum(p.numel() for p in model.backbone.parameters() if p.requires_grad)/1e6}
+
+    
+# with open(f'/vulcanscratch/sgirish/dummy/{key}_params.json','w') as f:
+#     json.dump(params,f)
+
+# exit()
+
+dataset_names = list(np.unique(np.array(dataset)))
+model_names = list(np.unique(np.array(list(imagenet_models.keys()))))
+
+nets = model_names
+accs = np.zeros((len(nets),len(dataset_names)))
+for i in range(len(acc)):
+    model_name, dataset_name = model[i], dataset[i]
+    if model[i] in nets:
+        accs[nets.index(model_name),dataset_names.index(dataset_name)] = acc[i]
+
+keep_model_idx = np.arange(accs.shape[0])[np.all(accs!=0.0,axis=1)]
+imagenet_models_subset = {k:v for k,v in imagenet_models.items() if model_names.index(k) in keep_model_idx}
+
+model_plot = [model[i] for i in range(len(acc)) if dataset[i]!='imagenet' and model[i] in imagenet_models_subset]
+dataset_plot = [dataset[i] for i in range(len(acc)) if dataset[i]!='imagenet' and model[i] in imagenet_models_subset]
+acc_plot_y = [acc[i] for i in range(len(acc)) if dataset[i]!='imagenet' and model[i] in imagenet_models_subset]
+acc_plot_x = [imagenet_models[model[i]] for i in range(len(acc)) if dataset[i]!='imagenet' and model[i] in imagenet_models_subset]
+
+
+df = pd.DataFrame(list(zip(acc_plot_x, acc_plot_y, dataset_plot)), \
+                    columns=['ImageNet Accuracy', 'Accuracy','Dataset'])
+plt.clf()
+plt.grid()
+num_cols = 5
+sns.set_style("darkgrid")
+# grid = sns.FacetGrid(df, col = "Dataset", col_wrap=5, sharey=False, sharex=True, col_order=[n for n in dataset_names if n!='imagenet'])
+# grid.map(sns.lmplot, "ImageNet Accuracy", "Accuracy")
+# grid.add_legend()
+g = sns.lmplot(x="ImageNet Accuracy", y="Accuracy", col="Dataset", col_order=[n for n in dataset_names if n!='imagenet'],
+               data=df, col_wrap=5, sharey=False, sharex=True)
+
+in_min = min(list(imagenet_models_subset.values()))
+in_max = max(list(imagenet_models_subset.values()))
+for i in range(10):
+    dataset_name = [n for n in dataset_names if n!='imagenet'][i]
+    # print(dataset_name, cur_accs.min(),cur_accs.max())
+    cur_accs = [a for i,a in enumerate(acc) if dataset[i]==dataset_name and model[i] in imagenet_models_subset]
+    g.axes[i].set_ylim((min(cur_accs)*0.998,max(cur_accs)*1.002))
+    g.axes[i].set_xlim((in_min,in_max))
+    plt.show()
+    plt.savefig(f'/vulcanscratch/sgirish/results_imagenet/downstream_accs_{key}.png')
+    
+if key == 'resnet':
+    l1 = [params[net]['l1'] for net in imagenet_models]
+    l2 = [params[net]['l2'] for net in imagenet_models]
+    l3 = [params[net]['l3'] for net in imagenet_models]
+    l4 = [params[net]['l4'] for net in imagenet_models]
+    bottom_params = [params[net]['bottom_params'] for net in imagenet_models]
+    top_params = [params[net]['top_params'] for net in imagenet_models]
+    ratio_params = [params[net]['top_params']/params[net]['bottom_params'] for net in imagenet_models]
+    net_params = [params[net]['params'] for net in imagenet_models]
+    accs = [imagenet_models[net] for net in imagenet_models]
+
+    df = pd.DataFrame(list(zip(bottom_params, top_params, ratio_params, net_params, accs)), \
+                            columns=['Bottom Params', 'Top Params', 'Top Params / Bottom Params', 'Net Params', f'ImageNet Accuracy'])
+    plt.clf()
+    g = sns.lmplot('Net Params', 'ImageNet Accuracy', df)
+    plt.show()
+    plt.savefig(f'/vulcanscratch/sgirish/results_imagenet/params_imagenet_{key}.png')
+elif key == 'mobilenet':
+    net_params = [params[net]['params'] for net in imagenet_models]
+    accs = [imagenet_models[net] for net in imagenet_models]
+    df = pd.DataFrame(list(zip(net_params, accs)), \
+                            columns=['Net Params', f'ImageNet Accuracy'])
+    plt.clf()
+    g = sns.lmplot('Net Params', 'ImageNet Accuracy', df)
+    plt.show()
+    plt.savefig(f'/vulcanscratch/sgirish/results_imagenet/params_imagenet_{key}.png')
+exit()
+
+
+
+
 
 api = wandb.Api()
 
