5ab572876604c15ef92a6893de98218d4ca7feab
Sharath Girish <sgirish@terpmail.umd.edu>
2021-11-06 11:51:46 -0400

    misc

--- scripts/ssl/plot_resnets.py
+++ scripts/ssl/plot_resnets.py
@@ -23,6 +23,11 @@ def param_plots(params, models, dataset_name, dataset, key):
     # l2 = [params[net]['l2'] for net in models]
     # l3 = [params[net]['l3'] for net in models]
     # l4 = [params[net]['l4'] for net in models]
+    if key == 'mobilenet':
+        nets = list(models.keys())
+        for net in nets:
+            if int(net.split('mobilenet_v')[-1])<12 and int(net.split('mobilenet_v')[-1])>1:
+                del models[net]
     bottom_params = [params[net]['bottom_params'] for net in models]
     top_params = [params[net]['top_params'] for net in models]
     vals, count = np.unique(np.array(bottom_params),return_counts=True)
@@ -65,7 +70,7 @@ def param_plots(params, models, dataset_name, dataset, key):
     plt.show()
     plt.savefig(f'/vulcanscratch/sgirish/results_imagenet/top_params_{dataset}_{key}.png')
 # key = 'resnet'
-key = 'mobilenet'
+key = 'resnet'
 with open(f'/vulcanscratch/sgirish/dummy/resnet_params.json','r') as f:
     resnet_params = json.load(f)
 with open(f'/vulcanscratch/sgirish/dummy/mobilenet_params.json','r') as f:
@@ -77,8 +82,11 @@ with open('/vulcanscratch/sgirish/eval_models.csv') as csv_file:
     imagenet_resnet_models = {}
     imagenet_mobilenet_models = {}
     for i,row in enumerate(csv_reader):
-        acc.append(float(row['epoch_top1_test']))
         name = row['Name']
+        # if 'mobilenet' in name.strip().split('-')[-1] and int(name.strip().split('-')[-1].split('mobilenet_v')[-1])>1 and \
+        #     int(name.strip().split('-')[-1].split('mobilenet_v')[-1])<12:
+        #     print(name)
+        acc.append(float(row['epoch_top1_test']))
         model.append(name.strip().split('-')[-1])
         dataset.append(name.strip().split('-')[0].split('_')[-1])
         if name.strip().split('-')[0].split('_')[-1] == 'imagenet':
@@ -86,9 +94,9 @@ with open('/vulcanscratch/sgirish/eval_models.csv') as csv_file:
                 imagenet_resnet_models[name.strip().split('-')[-1]] = float(row['epoch_top1_test'])
             elif 'mobilenet' in name.strip().split('-')[-1] and float(mobilenet_params[name.strip().split('-')[-1]]['params']<3):
                 imagenet_mobilenet_models[name.strip().split('-')[-1]] = float(row['epoch_top1_test'])
-
 imagenet_models = imagenet_resnet_models if key == 'resnet' else imagenet_mobilenet_models
 
+###!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!REMEMBER TO CHANGE c10r20 to c100r20 ON VULCAN!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 # params = {}
 # if key == 'resnet':
 #     with open('confs/algos/simclr_resnets.yaml', 'r') as f:
@@ -179,12 +187,17 @@ for i in range(10):
 plt.show()
 plt.savefig(f'/vulcanscratch/sgirish/results_imagenet/downstream_accs_{key}.png')
     
-cur_dataset = ['aircraft', 'flower102', 'cars196', 'imagenet']
-cur_dataset_names = ['Aircraft','Flowers102','Stanford Cars', 'ImageNet']
+cur_dataset = ['imagenet']+[n for n in dataset_names if n!='cifar10' and n!='imagenet']
+cur_dataset_names = {'aircraft':'Aircraft', 'flower102':'Flowers102', 'cars196':'Stanford Cars', 'imagenet':'ImageNet',
+                     'cifar10':'Cifar10', 'cifar100':'Cifar100', 'cub200':'Caltech Birds', 'dogs120':'Stanford Dogs',
+                     'mit67':'MIT67', 'sport8':'Sports', 'svhn':'SVHN'}
+cur_dataset_names = [cur_dataset_names[k] for k in cur_dataset]
 if key == 'resnet':
-    min_vals = {'aircraft':38.0, 'flower102':91.5, 'cars196':32.0, 'imagenet':54.0}
+    min_vals = {'aircraft':38.0, 'flower102':91.5, 'cars196':32.0, 'imagenet':54.0, 'cifar10':82.0, 'cifar100':59.0,
+                'cub200':28.0, 'dogs120':46.0, 'mit67':65.0, 'svhn':66.0, 'sport8':94.5}
 else:
-    min_vals = {'aircraft':0.0, 'flower102':0.0, 'cars196':0.0, 'imagenet':0.0}
+    min_vals = {'aircraft':0.0, 'flower102':0.0, 'cars196':0.0, 'imagenet':40.5, 'cifar10':0.0, 'cifar100':0.0,
+                'cub200':0.0, 'dogs120':0.0, 'mit67':0.0, 'svhn':0.0, 'sport8':0.0}
 max_params = 30 if key == 'resnet' else 3
 acc_y = [acc[i] for i in range(len(acc)) if dataset[i] in cur_dataset and key in model[i] and \
             params[model[i]]['params']<max_params and acc[i]>min_vals[dataset[i]]]
@@ -195,19 +208,18 @@ dataset_plot = [dataset[i] for i in range(len(acc)) if dataset[i] in cur_dataset
 df = pd.DataFrame(list(zip(params_x, acc_y, dataset_plot)), columns=['Params (in Million)', 'Accuracy','Dataset'])
 plt.clf()
 plt.grid()
-g = sns.lmplot(x="Params (in Million)", y="Accuracy", col="Dataset", col_order=cur_dataset[::-1],
-            data=df, col_wrap=2, sharey=False, sharex=True, truncate=False, order=2)
+g = sns.lmplot(x="Params (in Million)", y="Accuracy", col="Dataset", col_order=cur_dataset,
+            data=df, col_wrap=5, sharey=False, sharex=True, truncate=False, order=2)
 for j in range(len(cur_dataset)):
-    cur_accs = [acc[i] for i in range(len(acc)) if dataset[i] == cur_dataset[::-1][j] and key in model[i] and \
+    cur_accs = [acc[i] for i in range(len(acc)) if dataset[i] == cur_dataset[j] and key in model[i] and \
                 params[model[i]]['params']<max_params and acc[i]>min_vals[dataset[i]]]
     g.axes[j].set_xlim((min(params_x)*0.998,max(params_x)*1.002))
     g.axes[j].set_ylim((min(cur_accs)*0.998,max(cur_accs)*1.002))
 plt.show()
 plt.savefig(f'/vulcanscratch/sgirish/results_imagenet/downstream_params_{key}.png')
 
-param_plots(params, imagenet_models, 'ImageNet', 'imagenet', key)
-for plot_dataset, plot_dataset_name, min_acc in zip(['aircraft', 'flower102', 'cars196', 'imagenet'],\
-    ['Aircraft','Flowers102','Stanford Cars', 'ImageNet'],[38.0, 91.5, 32.0, 54.0]):
+# param_plots(params, imagenet_models, 'ImageNet', 'imagenet', key)
+for plot_dataset, plot_dataset_name, min_acc in zip(cur_dataset,cur_dataset_names,[min_vals[d] for d in cur_dataset]):
     cur_accs = {}
     cur_accs = {model[i]:acc[i] for i in range(len(acc)) if dataset[i]==plot_dataset and key in model[i] \
                                                             and params[model[i]]['params']<max_params and acc[i]>min_acc}
